fa3f521ce65d1a1adadbd25c1dc7c5be5fca0463
BooterMan98 <cesarpaulangelo@gmail.com>
2021-10-29 14:17:21 -0300

    feat: First comms
    Co-authored-by: tobi <tobimd@users.noreply.github.com>

--- leader/main.go
+++ leader/main.go
@@ -1,9 +1,28 @@
-package leader;
+package leader
 
 import (
-    "log"
+	"context"
+	"log"
+
+	pb "github.com/litneet64/lab-2-squid-game/protogrpc"
 )
 
+const (
+	port = ":50051"
+)
+
+type PlayerToLeaderRequestMsgType int32
+
+type server struct {
+	pb.UnimplementedGameInteractionServer
+}
+
+func (s *server) PlayerSend(ctx context.Context, in *pb.PlayerToLeaderRequest) (*pb.PlayerToLeaderReply, error) {
+	log.Printf("Received: %v", in.GetMsg())
+	return &pb.PlayerToLeaderReply{Msg: pb.PlayerToLeaderReply_ACCEPT_JOIN.Enum()}, nil
+}
+
 func Leader_go() {
-    log.Println("[+] Success!")
+
+	log.Println("[+] Success!")
 }
--- player/main.go
+++ player/main.go
@@ -1,14 +1,43 @@
-package player;
+package player
 
 import (
-    "log"
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/litneet64/lab-2-squid-game/protogrpc"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	address     = "localhost:50051" // Cambiar despues
+	defaultName = "leader"          // También
 )
 
 
 func Player_bot_go() {
     log.Println("[+] Hello from bot player!")
+
+	
 }
 
 func Player_human_go() {
     log.Println("[+] Hello from player!")
+    conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("No funca :( : %v", err)
+	} else {
+		log.Println("[+] Success!")
+	}
+	defer conn.Close()
+	client := pb.NewGameInteractionClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	response, err := client.PlayerSend(ctx, &pb.PlayerToLeaderRequest{Msg: pb.PlayerToLeaderRequest_JOIN_GAME.Enum()})
+	if err != nil {
+		log.Fatalf("Error de mensaje(?): %v", err)
+	}
+	log.Printf("Mensaje: %v", response.GetMsg())
 }
