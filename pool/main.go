9c17b9d9277689465ae734ea7080a1036cc73f34
BooterMan98 <cesarpaulangelo@gmail.com>
2021-11-04 00:22:08 -0300

    pool listener

--- pool/main.go
+++ pool/main.go
@@ -1,9 +1,57 @@
-package pool;
+package pool
 
 import (
-    "log"
+	"log"
+
+	"github.com/streadway/amqp"
 )
 
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
 func Pool_go() {
-    log.Println("[+] Success!")
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/") // replace with actual contents
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"money pool", // name
+		false,        // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+	log.Println("[+] Success!")
 }
