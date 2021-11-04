1ae5a36d7bde5e386bb143465b03e4fb491d8fd2
ecarrill <esteban.carrillo@sansano.usm.cl>
2021-11-04 02:38:16 -0300

    wip namenode & datanode

--- datanode/main.go
+++ datanode/main.go
@@ -1,9 +1,87 @@
-package datanode;
+package datanode
 
 import (
-    "log"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"bufio"
+
+	pb "github.com/litneet64/lab-2-squid-game/protogrpc"
+	"google.golang.org/grpc"
 )
 
+type RoundInfo struct {
+	playerId   uint32
+	playerMove uint32
+}
+
+type server struct {
+	pb.UnimplementedDataRegistryServiceServer
+}
+
+const namenodeAddr string = "localhost:50051"
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func createFile(player uint32, game uint32, moves []RoundInfo) {
+	f, err := os.Create(fmt.Sprintf("jugador_%d__etapa_%d.txt", player, game))
+	check(err)
+	defer f.Close()
+	for i := 0; i < len(moves); i++ {
+		if moves[i].playerId == player {
+			f.WriteString(fmt.Sprintf("%d\n", moves[i].playerMove))
+		}
+	}
+	f.Sync()
+}
+func appendRound(player uint32, game uint32, moves []RoundInfo) {
+	f, err := os.Open(fmt.Sprintf("jugador_%d__etapa_%d.txt", player, game))
+	check(err)
+	defer f.Close()
+	for i := 0; i < len(moves); i++ {
+		if moves[i].playerId == player {
+			f.WriteString(fmt.Sprintf("%d\n", moves[i].playerMove))
+		}
+	}
+	f.Sync()
+}
+
+func getGame(player uint32, game uint32) []RoundInfo {
+    var game []RoundInfo
+    f, err := os.Open(fmt.Sprintf("jugador_%d__etapa_%d.txt", player, game))
+    scanner := bufio.NewScanner(f)
+    for scanner.Scan() {
+        game.append(RoundInfo{playerId: player, playerMove: scanner.Int})
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+}
+
+    return game
+
+}
+
+
 func Datanode_go() {
-    log.Println("[+] Success!")
+	// Set the listening port for the server
+	lis, err := net.Listen("tcp", namenodeAddr)
+	if err != nil {
+		log.Fatalf("[Datanode] Could not listen: %v", err)
+	}
+
+	// Define and register new server for this datanode
+	datanodeServer := grpc.NewServer()
+	pb.RegisterDataRegistryServiceServer(datanodeServer, &server{})
+	
+	// Start listening
+	if err := datanodeServer.Serve(lis); err != nil {
+		log.Fatalf("[Datanode] Could not serve: %v", err)
+	}
 }
--- namenode/main.go
+++ namenode/main.go
@@ -1,9 +1,73 @@
-package namenode;
+package namenode
 
 import (
-    "log"
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/litneet64/lab-2-squid-game/protogrpc"
+	"google.golang.org/grpc"
 )
 
+type RoundInfo struct {
+	playerId   uint32
+	playerMove uint32
+}
+
+var (
+	datanodeAddr = [3]string{
+		"localhost:50051",
+		"localhost:50051",
+		"localhost:50051",
+	}
+)
+
+// Save the given round info into a txt file of the given datanode
+func Register(client pb.DataRegistryServiceClient, stage uint32, round uint32, roundInfo []RoundInfo) {
+	// Start timed context
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	// Store each move, using `roundInfo`
+	all_moves := make([]*pb.PlayersMoves_Move, len(roundInfo))
+
+	for i := 0; i < len(roundInfo); i++ {
+		all_moves[i] = &pb.PlayersMoves_Move{
+			PlayerId:   &roundInfo[i].playerId,
+			PlayerMove: &roundInfo[i].playerMove,
+		}
+	}
+
+	// Send message to datanode
+	client.TransferPlayerMoves(ctx,
+		&pb.PlayersMoves{
+			Stage:        &stage,
+			Round:        &round,
+			PlayersMoves: all_moves,
+		})
+}
+
+// Recieve round info from datanode
+func Retrieve() {
+
+}
+
 func Namenode_go() {
-    log.Println("[+] Success!")
+	// Define two arrays of both connections and errors for each of the
+	// three datanodes that are connected to the namenode
+
+	var conns [3]*grpc.ClientConn
+	var errs [3]error
+	var clients [3]pb.DataRegistryServiceClient
+
+	// Dial each datanode
+	for i := 0; i < 3; i++ {
+		conns[i], errs[i] = grpc.Dial(datanodeAddr[i], grpc.WithInsecure())
+		if errs[i] != nil {
+			log.Fatalf("[Namenode] Error connecting to datanode #%d: \"%v\"", i, errs[i])
+		}
+		clients[i] = pb.NewDataRegistryServiceClient(conns[i])
+		defer conns[i].Close()
+	}
+
 }
