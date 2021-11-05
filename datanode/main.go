48d50572c86920b32cad0f02b8eef4e4ad5825d9
BooterMan98 <cesarpaulangelo@gmail.com>
2021-11-04 22:01:57 -0300

    Updated datanode

--- datanode/main.go
+++ datanode/main.go
@@ -1,13 +1,15 @@
 package datanode
 
 import (
+	"bufio"
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
-	"bufio"
+	"strconv"
 
-	pb "github.com/litneet64/lab-2-squid-game/protogrpc"
+	pb "github.com/litneet64/lab-2-squid-stage/protogrpc"
 	"google.golang.org/grpc"
 )
 
@@ -22,32 +24,48 @@ type server struct {
 
 const namenodeAddr string = "localhost:50051"
 
-func check(e error) {
+func checkIfErr(e error) {
 	if e != nil {
-		panic(e)
+		log.Fatal(e)
 	}
 }
 
-func createFile(player uint32, game uint32, moves []RoundInfo) {
-	f, err := os.Create(fmt.Sprintf("jugador_%d__etapa_%d.txt", player, game))
-	check(err)
-	defer f.Close()
-	for i := 0; i < len(moves); i++ {
-		if moves[i].playerId == player {
-			f.WriteString(fmt.Sprintf("%d\n", moves[i].playerMove))
-		}
+// Writes player move to a file
+func writeToFile(player uint32, stage uint32, move uint32) {
+	var f *os.File
+	var err error
+
+	filename := fmt.Sprintf("jugador_%d__etapa_%d.txt", player, stage)
+
+	// Create if it doesn't exist, otherwise just open it
+	if _, err := os.Stat(filename); err == nil {
+		f, err = os.Create(filename)
+	} else {
+		f, err = os.Open(filename)
 	}
+	checkIfErr(err)
+	defer f.Close()
+
+	// Writes to file
+	f.WriteString(fmt.Sprintf("%d\n", move))
 	f.Sync()
 }
 
-func appendRound(player uint32, game uint32, moves []RoundInfo) {
-	f, err := os.Open(fmt.Sprintf("jugador_%d__etapa_%d.txt", player, game))
-	check(err)
-	defer f.Close()
-	for i := 0; i < len(moves); i++ {
-		if moves[i].playerId == player {
-			f.WriteString(fmt.Sprintf("%d\n", moves[i].playerMove))
-		}
+// Parses registered player moves from it's file and returns them
+func getPlayerStageRounds(player uint32, stage uint32) []uint32 {
+	// List of all player moves for a given stage
+	var moves []uint32
+
+	// Read player file for the given stage
+	f, fErr := os.Open(fmt.Sprintf("jugador_%d__etapa_%d.txt", player, stage))
+	checkIfErr(fErr)
+
+	// Start saving each move
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		move, err := strconv.Atoi(scanner.Text())
+		checkIfErr(err)
+		moves = append(moves, uint32(move))
 	}
 	f.Sync()
 }
@@ -61,14 +79,20 @@ func getGame(player uint32, game uint32) []RoundInfo {
         game.append(RoundInfo{playerId: player, playerMove: scanner.Int}) // puede fallar
     }
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
-    return game
+	return moves
 }
 
 // The server-side implementation of the rpc function that the namenode calls
 func (s *server) TransferPlayerMoves(ctx context.Context, in *pb.PlayersMoves) (*pb.Empty, error) {
+	moves := in.GetPlayersMoves()
+	stage := in.GetStage()
+
+	// For each move, append it to the corresponding file
+	for i := 0; i < len(moves); i++ {
+		writeToFile(moves[i].GetPlayerId(), stage, moves[i].GetPlayerMove())
+	}
+
+	// No reply is expected, so return empty message
 	return &pb.Empty{}, nil
 }
 
@@ -82,9 +106,10 @@ func Datanode_go() {
 	// Define and register new server for this datanode
 	datanodeServer := grpc.NewServer()
 	pb.RegisterDataRegistryServiceServer(datanodeServer, &server{})
-	
+
 	// Start listening
 	if err := datanodeServer.Serve(lis); err != nil {
 		log.Fatalf("[Datanode] Could not serve: %v", err)
 	}
+
 }
