71f2b5361f3114e77a53d830dd46586740ede69a
ecarrill <esteban.carrillo@sansano.usm.cl>
2021-11-06 02:03:56 -0300

    feat: add pool functionality
    Co-authored-by: litneet64 <litneet64@gmail.com>
    Co-authored-by: BooterMan98 <cesarpaulangelo@gmail.com>

--- pool/main.go
+++ pool/main.go
@@ -1,25 +1,119 @@
 package pool
 
 import (
+	"bufio"
+	"context"
+	"fmt"
 	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
 
+	pb "github.com/litneet64/lab-2-squid-game/protogrpc"
 	"github.com/streadway/amqp"
+	"google.golang.org/grpc"
 )
 
-func failOnError(err error, msg string) {
+const (
+	leaderAddr = "localhost:50051"
+	leaderGrpc = "localhost:50052"
+)
+
+type server struct {
+	pb.UnimplementedPrizeServer
+}
+
+func FailOnError(err error, msg string) {
 	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
+		log.Fatalf("[Error]: (%v) %s", err, msg)
 	}
 }
 
-func Pool_go() {
+func Send()
 
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/") // replace with actual contents
-	failOnError(err, "Failed to connect to RabbitMQ")
+// writes last player death and new pool prize
+func RegisterPlayerDeath(player uint32, stage uint32) {
+	var f *os.File
+	var err error
+	var money uint32
+
+	_, fErr := os.Stat("pool.txt")
+	if fErr != nil {
+		f, err = os.Create("pool.txt")
+		money = 0
+	}
+
+	f, err = os.Open("pool.txt")
+
+	FailOnError(err, "can't open file \"pool.txt\"")
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		moneyint, mErr := strconv.Atoi(strings.Split(scanner.Text(), " ")[2])
+		FailOnError(mErr, "error")
+		money = uint32(moneyint)
+	}
+
+	_, wErr := f.WriteString(fmt.Sprintf("Jugador_%d Ronda_%d %d", player, stage, money+100))
+	FailOnError(wErr, "error")
+
+	f.Sync()
+}
+
+// return pool prize
+func RetrievePrice() (prize uint32) {
+	prize = 0
+
+	_, fErr := os.Stat("pool.txt")
+	if fErr != nil {
+		return 0
+	}
+	f, err := os.Open("pool.txt")
+
+	FailOnError(err, "can't open file \"pool.txt\"")
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		moneyint, mErr := strconv.Atoi(strings.Split(scanner.Text(), " ")[2])
+		FailOnError(mErr, "error")
+		prize = uint32(moneyint)
+	}
+
+	return
+}
+
+func setupPoolServer() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(leaderGrpc))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterPrizeServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func (s *server) GetPrize(ct context.Context, in *pb.CurrentPoolRequest) (*pb.CurrentPoolReply, error) {
+	price := RetrievePrice()
+
+	return &pb.CurrentPoolReply{CurrPrize: &price}, nil
+}
+
+func Pool_go() {
+	// grpc conection
+	go setupPoolServer()
+	// Dial Leader
+	conn, err := amqp.Dial(leaderAddr)
+	FailOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
 	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
+	FailOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
@@ -30,7 +124,7 @@ func Pool_go() {
 		false,        // no-wait
 		nil,          // arguments
 	)
-	failOnError(err, "Failed to declare a queue")
+	FailOnError(err, "Failed to declare a queue")
 
 	msgs, err := ch.Consume(
 		q.Name, // queue
@@ -41,13 +135,16 @@ func Pool_go() {
 		false,  // no-wait
 		nil,    // args
 	)
-	failOnError(err, "Failed to register a consumer")
+	FailOnError(err, "Failed to register a consumer")
 
 	forever := make(chan bool)
 
 	go func() {
 		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
+			text := strings.Split(fmt.Sprint(d.Body), " ")
+			playerId, _ := strconv.Atoi(text[0])
+			stage, _ := strconv.Atoi(text[1])
+			RegisterPlayerDeath(uint32(playerId), uint32(stage))
 		}
 	}()
 
