42ca47e673212747361dd3412c644c34e4af282b
ecarrill <esteban.carrillo@sansano.usm.cl>
2021-11-06 02:06:03 -0300

    fix: add rmissing fields in proto
    Co-authored-by: litneet64 <litneet64@gmail.com>
    Co-authored-by: BooterMan98 <cesarpaulangelo@gmail.com>

--- main.go
+++ main.go
@@ -1,40 +1,40 @@
-package main;
+package main
 
 import (
-    "log"
-    "os"
-    "github.com/litneet64/lab-2-squid-game/leader"
-    "github.com/litneet64/lab-2-squid-game/player"
-    "github.com/litneet64/lab-2-squid-game/datanode"
-    "github.com/litneet64/lab-2-squid-game/namenode"
-    "github.com/litneet64/lab-2-squid-game/pool"
+	"log"
+	"os"
+
+	"github.com/litneet64/lab-2-squid-game/datanode"
+	"github.com/litneet64/lab-2-squid-game/leader"
+	"github.com/litneet64/lab-2-squid-game/namenode"
+	"github.com/litneet64/lab-2-squid-game/player"
+	"github.com/litneet64/lab-2-squid-game/pool"
 )
 
 func show_help() {
-  log.Fatalf("[!] Usage: %s <role [leader/player_bot/player/namenode/datanode/pool]>", os.Args[0])
+	log.Fatalf("[!] Usage: %s <role [leader/player_bot/player/namenode/datanode/pool]>", os.Args[0])
 }
 
-
 func main() {
-    if len(os.Args[:]) < 2 {
-      show_help()
-    }
+	if len(os.Args[:]) < 2 {
+		show_help()
+	}
 
-    switch cmd := os.Args[1]; cmd {
-      case "leader":
-        leader.Leader_go()
-      case "player_bot":
-        player.Player_go("bot")
-      case "player":
-        player.Player_go("human")
-      case "namenode":
-        namenode.Namenode_go()
-      case "datanode":
-        datanode.Datanode_go()
-      case "pool":
-        pool.Pool_go()
-      default:
-        show_help()
-    }
+	switch cmd := os.Args[1]; cmd {
+	case "leader":
+		leader.Leader_go()
+	case "player_bot":
+		player.Player_go("bot")
+	case "player":
+		player.Player_go("human")
+	case "namenode":
+		namenode.Namenode_go()
+	case "datanode":
+		datanode.Datanode_go()
+	case "pool":
+		pool.Pool_go()
+	default:
+		show_help()
+	}
 
 }
